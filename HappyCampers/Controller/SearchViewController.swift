2ec67923f7a6ed14fc11a5012148a6524482cb90
Rehan Parkar <parkarcamp@gmail.com>
2018-04-08 15:49:01 -0500

    Added all basic UI elements

---
+++ HappyCampers/Controller/SearchViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SearchViewController.swift
+//  HappyCampers
+//
+//  Created by Rehan Parkar on 2018-04-08.
+//  Copyright © 2018 Rehan Parkar. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+   
+
+}
