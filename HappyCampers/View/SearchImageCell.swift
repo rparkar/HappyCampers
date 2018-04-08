2b56bd3dd48571adef36746ae05edcd697931590
Rehan Parkar <parkarcamp@gmail.com>
2018-04-08 16:09:59 -0500

    Added navigation between view controllers and custom collection cells

---
+++ HappyCampers/View/SearchImageCell.swift
@@ -0,0 +1,13 @@
+//
+//  SearchImageCell.swift
+//  HappyCampers
+//
+//  Created by Rehan Parkar on 2018-04-08.
+//  Copyright © 2018 Rehan Parkar. All rights reserved.
+//
+
+import UIKit
+
+class SearchImageCell: UICollectionViewCell {
+    
+}
